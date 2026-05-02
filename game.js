2c79fb57ae0bf39eb3c5b889bfdfd1bdbdd130fd
JasonGarabedian483 <jrgarabe@ucsc.edu>
2026-05-01 21:16:18 -0700

    Added button sound effect when clicking navigation arrows by suggestion in class. Took inspiration from Quetzals use of sound effects in his game.

--- game.js
+++ game.js
@@ -242,7 +242,6 @@ class kitchenScene extends AdventureScene {
         this.load.image('kitchen', 'kitchenimage.jpg')
         this.load.image('backarrow', 'leftarrow.png')
         this.load.image('plate', 'plate.png')
-        this.load.audio('button', 'button.mp3')
     }
 
     onEnter() {
@@ -259,7 +258,6 @@ class kitchenScene extends AdventureScene {
             })
             backArrow.on("pointerout", () => this.getSmaller(backArrow));
             backArrow.on("pointerdown", () => {
-                this.sound.play('button');
                 this.gotoScene('Livingroom');
             })
 
